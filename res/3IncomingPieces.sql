0cb84af889557625d5c702476292bae32b0415f5
LuccaGarcia <70383956+LuccaGarcia@users.noreply.github.com>
2024-05-16 18:07:36 +0100

    shop update

--- res/3IncomingPieces.sql
+++ res/3IncomingPieces.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS
     Incoming(
-        piece_id SERIAL PRIMARY KEY,
+        incoming_id SERIAL PRIMARY KEY,
         piece_type INTEGER NOT NULL,
         arrival_date INTEGER NOT NULL,
         piece_status varchar(20) NOT NULL,
-        cost INTEGER NOT NULL
+        cost INTEGER NOT NULL,
+        order_id INTEGER REFERENCES Orders(order_id)
     );
--- src/erp/warehouse/warehouse.py
+++ src/erp/warehouse/warehouse.py
@@ -8,12 +8,34 @@ def getFreePieces(conn, piece_type):
     
     return result[0]
 
+def alocatePieceToOrder(conn, order_id, piece_type):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Warehouse WHERE piece_type = %s AND piece_status = 'Free' LIMIT 1;", (piece_type,))
+    piece = cur.fetchone()
+
+    cur.execute("UPDATE Warehouse SET piece_status = 'Allocated' WHERE piece_id = %s;", (piece[0],))
+    conn.commit()
+    cur.execute("UPDATE Pieces SET order_id = %s WHERE piece_id = %s;", (order_id, piece[0],))
+    conn.commit()
+    print("Avaiable Piece", piece[0], "allocated to order", order_id)
+    
+def alocateIncomingPieceToOrders(conn, order_id, piece_type):
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM Incoming WHERE piece_type = %s AND order_id IS NULL LIMIT 1;", (piece_type,))
+    piece = cur.fetchone()
+    cur.execute("UPDATE Incoming SET order_id = %s WHERE incoming_id = %s;", (order_id, piece[0],))
+    conn.commit()
+    print("Incoming piece", piece[0], "allocated to order", order_id)
+    # cur.execute("UPDATE Warehouse SET piece_status = 'Allocated' WHERE piece_id = %s;", (piece[0],))
+    
+
 def placeBuyorder(conn, piece_type, quantity, current_day):
     cur = conn.cursor()
     #select from material costs
-    cur.execute("SELECT * FROM MaterialCosts WHERE piece = %s AND supplier = 3;", (piece_type,))
-    
+    cur.execute("SELECT * FROM MaterialCosts WHERE piece = %s AND supplier = 3;", (piece_type,))    
     selected_vendor = cur.fetchall()
+    
+    
     if quantity < selected_vendor[0][3]:
         quantity = selected_vendor[0][3]
     
@@ -31,7 +53,7 @@ def setPiecesToSpawn(conn, current_day):
               
     for piece in cur.fetchall():
         print("Requesting piece to spawn")
-        cur.execute("UPDATE Incoming SET piece_status = 'ToSpawn' WHERE piece_id = %s;", (piece[0], ))
+        cur.execute("UPDATE Incoming SET piece_status = 'ToSpawn' WHERE incoming_id = %s;", (piece[0], ))
         conn.commit()
         
 
@@ -39,16 +61,21 @@ def createAndPlaceSpawnedPiecesInWarehouse(conn):
     cur = conn.cursor()
     cur.execute("SELECT * FROM Incoming WHERE piece_status = 'Spawned';")
     
-    for piece in cur.fetchall():
+    #TODO: mark piece as alocated if order_id is not null
+    
+    
+    
+    for incoming_piece in cur.fetchall():
         
-        print("New piece spawned, adding to database.")
+        print( incoming_piece)
+        piece_status = 'Free' if incoming_piece[5] == None else 'Allocated'
         
-        cur.execute("INSERT INTO pieces (current_piece_type, accumulated_cost) VALUES (%s, %s) RETURNING piece_id;", (piece[1], piece[4]))
+        print("New piece spawned, adding to database.")        
+        cur.execute("INSERT INTO pieces (current_piece_type, accumulated_cost, order_id) VALUES (%s, %s, %s) RETURNING piece_id;", (incoming_piece[1], incoming_piece[4], incoming_piece[5]))
         #get the id of the inserted piece
         piece_id = cur.fetchone()[0]
         
         print("New piece inserted into warehouse")
-        
-        cur.execute("INSERT INTO Warehouse (warehouse, piece_id, piece_type, piece_status) VALUES (%s, %s, %s, 'Free');", (1, piece_id, piece[1],))
-        cur.execute("DELETE FROM Incoming WHERE piece_id = %s;", (piece[0],))
+        cur.execute("INSERT INTO Warehouse (warehouse, piece_id, piece_type, piece_status) VALUES (%s, %s, %s, %s);", (1, piece_id, incoming_piece[1],piece_status,))
+        cur.execute("DELETE FROM Incoming WHERE incoming_id = %s;", (incoming_piece[0],))
         conn.commit()
